8cb9d9de17fb2a312571763de184539066d8e699
angeliski <angeliski@hotmail.com>
2015-02-22 16:32:42 -0300

    commit inicial

---
+++ modules/tomcat/manifests/server.pp
@@ -0,0 +1,30 @@
+class tomcat::server($connectors = [],$data_sources = []){
+	package { ["tomcat7"]:
+		ensure => installed,
+		require => Exec["apt-update"],
+	}
+	file { "/var/lib/tomcat7/conf/server.xml":
+		owner => root,
+		group => tomcat7,
+		mode => 0644,
+		content => template("tomcat/server.xml"),
+		require => Package["tomcat7"],
+		notify => Service["tomcat7"],
+	}
+	file { "/etc/default/tomcat7":
+		owner => root,
+		group => root,
+		mode => 0644,
+		source => "puppet:///modules/tomcat/tomcat7",
+		require => Package["tomcat7"],
+		notify => Service["tomcat7"],
+	}
+	service { "tomcat7":
+		ensure => running,
+		enable => true,
+		hasstatus => true,
+		hasrestart => true,
+		require => Package["tomcat7"],
+	}
+
+}
